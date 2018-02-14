0f61b8c60263fabad05fab72edc469a8df43856b
AdolfH <adolfo.castaneda@ativar.com.mx>
2018-02-14 02:44:10 -0600

    joyeria.php V.1
    Implementacion del archivo joyeria.php para la pestaña Joyeria

---
+++ joyeria.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+            <meta content="IE=edge" http-equiv="X-UA-Compatible">
+                <title>
+                    Joyeria
+                </title>
+                <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet"></link>
+                <link href="https://fonts.googleapis.com/css?family=Courgette|Open+Sans+Condensed:300,300i|Philosopher|Playfair+Display:400,700|Poiret+One|Quattrocento+Sans" rel="stylesheet"></link>
+                <link href="./css/style-main.css" rel="stylesheet"></link>
+                <link href="./css/style-icon.css" rel="stylesheet" type="text/css"></link>
+                <link rel="stylesheet" type="text/css" href="./css/ihover.css"></link>
+                <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css"></link>
+            </meta>
+        </meta>
+        <style type="text/css">
+            .contenedor {
+              position: relative;
+              height: 100%;
+              width: 100%;
+             
+            }
+
+            .contenedor img {
+              position: absolute;
+              left: 0;
+              height: 100%;
+              width: 100%;
+              transition: opacity 1s ease-in-out;
+            }
+
+            .contenedor img.top:hover {
+              height: 100%;
+              width: 100%;
+              opacity: 0;
+            }
+        </style>
+     </head>
+    <body>
+        <?php
+            include("./header.php");
+        ?> 
+        
+        <br>
+        <br>
+        <br>
+        <br>
+<section class="section-main section-movil">
+<div class="container">
+  <div class="row justify-content-md-center">
+
+    <div class="col-sm">
+      <div id="container-item" class="ih-item circle colored effect3 bottom_to_top contenedor">
+                        <a href="./item.html" title="">
+                            <div class="spinner"></div>
+                            <div class="img">
+                                <img src="./img/2-2.png" />
+                                <img class="top" src="./img/3-2.png" alt="img">
+                            </div>
+                            <div class="info">
+                              <h3>Corazón de Oro</h3>
+                              <p>Oro rosa de 10K
+                              Modelo: COAB01
+                              $3,500 MXN
+                          IVA INCLUIDO</p>
+                            </div>
+                        </a>
+                    </div>
+    </div>
+
+    <div class="col-sm">
+      <div id="container-item" class="ih-item circle colored effect3 bottom_to_top contenedor">
+                        <a href="./item.html" title="">
+                            <div class="spinner"></div>
+                            <div class="img">
+                                <img src="./img/2-2.png" />
+                                <img class="top" src="./img/3-2.png" alt="img">
+                            </div>
+                            <div class="info">
+                              <h3>Corazón de Oro</h3>
+                              <p>Oro rosa de 10K
+                              Modelo: COAB01
+                              $3,500 MXN
+                          IVA INCLUIDO</p>
+                            </div>
+                        </a>
+                    </div>
+    </div>
+
+    <div class="col-sm">
+       <div id="container-item" class="ih-item circle colored effect3 bottom_to_top contenedor">
+                        <a href="./item.html" title="">
+                            <div class="spinner"></div>
+                            <div class="img">
+                                <img src="./img/2-2.png" />
+                                <img class="top" src="./img/3-2.png" alt="img">
+                            </div>
+                            <div class="info">
+                              <h3>Corazón de Oro</h3>
+                              <p>Oro rosa de 10K
+                              Modelo: COAB01
+                              $3,500 MXN
+                          IVA INCLUIDO</p>
+                            </div>
+                        </a>
+                    </div>
+    </div>
+
+    <div class="col-sm">
+       <div id="container-item" class="ih-item circle colored effect3 bottom_to_top contenedor">
+                        <a href="./item.html" title="">
+                            <div class="spinner"></div>
+                            <div class="img">
+                                <img src="./img/2-2.png" />
+                                <img class="top" src="./img/3-2.png" alt="img">
+                            </div>
+                            <div class="info">
+                              <h3>Corazón de Oro</h3>
+                              <p>Oro rosa de 10K
+                              Modelo: COAB01
+                              $3,500 MXN
+                          IVA INCLUIDO</p>
+                            </div>
+                        </a>
+                    </div>
+    </div>
+
+  </div>
+
+  <br>
+  <br>
+  <br>
+
+  <div class="row justify-content-md-center">
+
+    <div class="col-sm">
+      <div id="container-item" class="ih-item circle colored effect3 bottom_to_top contenedor">
+                        <a href="./item.html" title="">
+                            <div class="spinner"></div>
+                            <div class="img">
+                                <img src="./img/2-2.png" />
+                                <img class="top" src="./img/3-2.png" alt="img">
+                            </div>
+                            <div class="info">
+                              <h3>Corazón de Oro</h3>
+                              <p>Oro rosa de 10K
+                              Modelo: COAB01
+                              $3,500 MXN
+                          IVA INCLUIDO</p>
+                            </div>
+                        </a>
+                    </div>
+    </div>
+
+    <div class="col-sm">
+      <div id="container-item" class="ih-item circle colored effect3 bottom_to_top contenedor">
+                        <a href="./item.html" title="">
+                            <div class="spinner"></div>
+                            <div class="img">
+                                <img src="./img/2-2.png" />
+                                <img class="top" src="./img/3-2.png" alt="img">
+                            </div>
+                            <div class="info">
+                              <h3>Corazón de Oro</h3>
+                              <p>Oro rosa de 10K
+                              Modelo: COAB01
+                              $3,500 MXN
+                          IVA INCLUIDO</p>
+                            </div>
+                        </a>
+                    </div>
+    </div>
+
+    <div class="col-sm">
+       <div id="container-item" class="ih-item circle colored effect3 bottom_to_top contenedor">
+                        <a href="./item.html" title="">
+                            <div class="spinner"></div>
+                            <div class="img">
+                                <img src="./img/2-2.png" />
+                                <img class="top" src="./img/3-2.png" alt="img">
+                            </div>
+                            <div class="info">
+                              <h3>Corazón de Oro</h3>
+                              <p>Oro rosa de 10K
+                              Modelo: COAB01
+                              $3,500 MXN
+                          IVA INCLUIDO</p>
+                            </div>
+                        </a>
+                    </div>
+    </div>
+
+    <div class="col-sm">
+       <div id="container-item" class="ih-item circle colored effect3 bottom_to_top contenedor">
+                        <a href="./item.html" title="">
+                            <div class="spinner"></div>
+                            <div class="img">
+                                <img src="./img/2-2.png" />
+                                <img class="top" src="./img/3-2.png" alt="img">
+                            </div>
+                            <div class="info">
+                              <h3>Corazón de Oro</h3>
+                              <p>Oro rosa de 10K
+                              Modelo: COAB01
+                              $3,500 MXN
+                          IVA INCLUIDO</p>
+                            </div>
+                        </a>
+                    </div>
+    </div>
+
+  </div>
+
+
+
+</div>
+
+
+
+
+       </section>
+        <?php
+            include("./footer.php");
+        ?>
+    </body>
+</html>
